9429d6882acaa8f4d53f23cb8b5a348737dcb773
Audrey457 <laudrey457@gmail.com>
2024-01-03 11:13:27 +0100

    lire fichier csv

--- HelloSpark.py
+++ HelloSpark.py
@@ -1,8 +1,10 @@
+import sys
+
 from pyspark import SparkConf
 from pyspark.sql import *
 
 from lib.logger import Log4J
-from lib.utils import get_spark_app_config
+from lib.utils import get_spark_app_config, load_survey_df
 
 if __name__ == "__main__":
     # En utilisant un fichier de config
@@ -20,9 +22,19 @@ if __name__ == "__main__":
     #     .master(("local[3]")) \
     #     .getOrCreate()
 
+
     logger = Log4J(spark)
+
+    if len(sys.argv) != 2:
+        logger.error("Usage: HelloSpark <filename>")
+        sys.exit(-1)
+
     logger.info("Starting HelloSpark")
-    conf_out = spark.sparkContext.getConf()
-    logger.info(conf_out.toDebugString())
+    # conf_out = spark.sparkContext.getConf()
+    # logger.info(conf_out.toDebugString())
+
+    titanic_df = load_survey_df(spark, sys.argv[1])
+    titanic_df.show()
+
     logger.info("Finished HelloSpark")
     spark.stop()
